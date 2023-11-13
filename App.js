313dd65194574a6aa23d6b7cffc4ff5e62c6ea3a
Preeti Tandon <preeti.tandon@angloamerican.com>
2023-11-13 16:15:32 +0100

    create nested elements using react.creatElement

--- App.js
+++ App.js
@@ -1,9 +1,16 @@
-const heading = React.createElement(
-  "h1",
-  { id: "heading" },
-  "Hello World from React!"
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", { id: "heading" }, "Hello World from React!"),
+    React.createElement(
+      "h1",
+      { id: "heading2" },
+      "Hello World from React Again!"
+    ),
+  ])
 )
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 
-root.render(heading)
+root.render(parent)
