cd0102b1153b8e48e2b89ec48e2a0532ebaa5a21
Preeti Tandon <preeti.tandon@angloamerican.com>
2023-11-17 16:25:46 +0100

    remove int to roman function from App.js

--- App.js
+++ App.js
@@ -1,15 +1,4 @@
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", { id: "heading" }, "Hello World from React!"),
-    React.createElement(
-      "h1",
-      { id: "heading2" },
-      "Hello World from React Again!"
-    ),
-  ])
-)
+const parent = React.createElement("h1", {}, "Hello from React")
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 
